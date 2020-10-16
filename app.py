898124e4b095a0288687055fb86e72b0c7a81028
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-10-16 14:33:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def webhook():
 
   # We don't want to reply to ourselves!
   if data['name'] != 'Pythonbot':
-	msg = "what up bro"
+	msg = data
     #msg = '{}, you sent "{}".'.format(data['name'], data['text'])
     #comment
     #msg = "Why hello there"
