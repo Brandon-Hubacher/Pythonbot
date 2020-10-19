e746708cbc98bd2d82302ee5656a147a605805e1
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-10-18 20:34:17 -0500

    Update MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -3,14 +3,14 @@ import java.io.File;
 public class MainMethod {
     public static void main(String[] args)
     {
-        File f = new File("C:\\Users\\brandon\\Desktop\\GroupMeOrganizedData.json");
+        File f = new File(); //"C:\\Users\\brandon\\Desktop\\GroupMeOrganizedData.json"
         LikeMapMaker l = new LikeMapMaker();
-        l.setMap(f); //"C:\\Users\\brandon\\Desktop\\GroupMeOrganizedData.json"
+        l.setMap(f);
 
-        //File file = new File("C:\\Users\\brandon\\Desktop\\gmdata\\61672113\\message.json");
+        //File file = new File(); //"C:\\Users\\brandon\\Desktop\\gmdata\\61672113\\message.json"
         //FormatFile formatFileInstance = new FormatFile(file);
 
-        File convoFile = new File("C:\\Users\\brandon\\Desktop\\gmdata\\61672113\\conversation.json");
+        File convoFile = new File(); //"C:\\Users\\brandon\\Desktop\\gmdata\\61672113\\conversation.json"
         IDAndName IDAndNameInstance = new IDAndName();
         IDAndNameInstance.setPersonMap(convoFile);
     }
