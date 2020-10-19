e81ebcf8b557e837aa6b334f09ef4d60094d5e64
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-10-19 14:34:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import json
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+from FormatFilePY import personDict
 
 from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -15,43 +17,47 @@ def webhook():
   log('Recieved {}'.format(data))
 
   # We don't want to reply to ourselves!
-  if data['name'] != 'Pythonbot':
+  #if personDict.has_key(data[id()])
+
+  if data['name'] != 'Pythonbot' and data['id'] in personDict:
     msg = "WORK"
+
     if data['text'] == "!LR":
-      msg = "What up loser"
-      #get rank for corresponding ID
+      msg = data['name']+" LR:",personDict.get(data['id'].__getitem__(1))
+    elif data['text'] == "!LR rank":
+      msg = data['name']+" LR rank:",personDict.get(data['id'].__getitem__(2))
     elif data['text'] == "!LG":
-      #get rank for corresponding ID
-    elif data['text'] == "!LRrank":
-      #get rank for corresponding ID
-    elif data['text'] == "!LGrank":
-      #get rank for corresponding ID
+      msg = data['name']+" LG:",personDict.get(data['id'].__getitem__(3))
+    elif data['text'] == "!LG rank":
+      msg = data['name']+" LG rank:",personDict.get(data['id'].__getitem__(4))
     elif data['text'] == "!LRPerLG":
-      #get rank for corresponding ID
+      msg = data['name']+" LR per LG:",personDict.get(data['id'].__getitem__(5))
     elif data['text'] == "!LRPerLGrank":
-      #get rank for corresponding ID
+      msg = data['name']+" LR per LG rank:",personDict.get(data['id'].__getitem__(6))
     elif data['text'] == "!LRPerMS":
-      #get rank for corresponding ID
+      msg = data['name']+" LR per MS:",personDict.get(data['id'].__getitem__(7))
     elif data['text'] == "!LRPerMSrank":
-      #get rank for corresponding ID
+      msg = data['name']+" LR per MS rank:",personDict.get(data['id'].__getitem__(8))
     elif data['text'] == "!help":
-      msg = "!LR"+"\n"+"!LG"+"\n"+"!LRrank"+"\n"+"!LGrank"+"\n"+"!LRPerLG"+"\n"+"!LRPerLGrank"+"\n"+"!LRPerMS"+"\n"+"!LRPerMSrank"+"\n"+"!help"
+      msg = "!LR" + "\n" + "!LG" + "\n" + "!LRrank" + "\n" + "!LGrank" + "\n" + "!LRPerLG" + "\n" + "!LRPerLGrank" + "\n" + "!LRPerMS" + "\n" + "!LRPerMSrank" + "\n" + "!help"
   send_message(msg)
-    #msg = 'Recieved {}'.format(data))
-    #msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+  # msg = 'Recieved {}'.format(data))
+  # msg = '{}, you sent "{}".'.format(data['name'], data['text'])
 
   return "ok", 200
 
+
 def send_message(msg):
-  url  = 'https://api.groupme.com/v3/bots/post'
+  url = 'https://api.groupme.com/v3/bots/post'
 
   data = {
-          'bot_id' : os.getenv('GROUPME_BOT_ID'),
-          'text'   : msg,
-         }
+    'bot_id': os.getenv('GROUPME_BOT_ID'),
+    'text': msg,
+  }
   request = Request(url, urlencode(data).encode())
   json = urlopen(request).read().decode()
-  
+
+
 def log(msg):
   print(str(msg))
   sys.stdout.flush()
