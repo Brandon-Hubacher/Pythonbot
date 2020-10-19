cc08c6db6c0330f68bc4d8eb63bf61c35f50efe3
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-10-18 20:36:48 -0500

    Update MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -3,12 +3,12 @@ import java.io.File;
 public class MainMethod {
     public static void main(String[] args)
     {
+        //File file = new File("Python bot groupme data/63363295/message.json"); //"C:\\Users\\brandon\\Desktop\\gmdata\\61672113\\message.json"
+        //FormatFile formatFileInstance = new FormatFile(file);
+        
         File f = new File(); //"C:\\Users\\brandon\\Desktop\\GroupMeOrganizedData.json"
         LikeMapMaker l = new LikeMapMaker();
         l.setMap(f);
-
-        //File file = new File(); //"C:\\Users\\brandon\\Desktop\\gmdata\\61672113\\message.json"
-        //FormatFile formatFileInstance = new FormatFile(file);
 
         File convoFile = new File(); //"C:\\Users\\brandon\\Desktop\\gmdata\\61672113\\conversation.json"
         IDAndName IDAndNameInstance = new IDAndName();
