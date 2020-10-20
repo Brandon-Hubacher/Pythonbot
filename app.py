f99b7e9f3310bb784472f40e66ca07c6aef47ee6
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-10-20 16:11:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,23 @@ from urllib.request import Request, urlopen
 
 from flask import Flask, request
 
+def getPersonDict():
+      personDict = {}
+      # FinalPersonDataPYTHON.json
+      # "C:\\Users\\brandon\\Desktop\\FinalPersonData.json"
+
+      with open("FinalPersonDataPYTHON.json", 'r') as rf:
+          for line in rf:
+              variableList = line.split(',')
+              id = variableList[0]
+              variableList.pop(0)
+              personDict[id] = variableList
+
+        
+      return personDict
+    
+dict = getPersonDict()    
+
 app = Flask(__name__)
 
 #dict = getPersonDict()
@@ -23,50 +40,27 @@ def webhook():
   #if personDict.has_key(data[id()])
   #FormatFilePY.makePersonDict()
   if data['name'] != 'Pythonbot':
-    def getPersonDict():
-      personDict = {}
-      # FinalPersonDataPYTHON.json
-      # "C:\\Users\\brandon\\Desktop\\FinalPersonData.json"
-
-      with open("FinalPersonDataPYTHON.json", 'r') as rf:
-          for line in rf:
-              variableList = line.split(',')
-              id = variableList[0]
-              variableList.pop(0)
-              personDict[id] = variableList
-
-          #print(personDict.get("35762170"))
-          #print(personDict.values())
-          #print(personDict.get("35762170").__getitem__(8))
-      return personDict
-    
-    dict = getPersonDict()
     msg = "recognizes you sent message"
-    #if data['id'] in dict.keys():
-    if data['text'] == "!LR":
-      #msg = "hi "+len(dict).__str__()
-      msg = data['name']+" LR:",dict.get(data['sender_id']).__getitem__(1)
-      #msg = "hi"
-      #msg = "hi "+data['id']
-    elif data['text'] == "!LR rank":
-      #msg = data['name']+" LR rank:",dict.get(data['id'].__getitem__(2))
-      #msg = "hi "+data['id]
-      msg = "what LG"
-    elif data['text'] == "!LG":
-      msg = data['name']+" LG:",dict.get(data['id'].__getitem__(3))
-    elif data['text'] == "!LG rank":
-      msg = data['name']+" LG rank:",dict.get(data['id'].__getitem__(4))
-    elif data['text'] == "!LRPerLG":
-      msg = data['name']+" LR per LG:",dict.get(data['id'].__getitem__(5))
-    elif data['text'] == "!LRPerLGrank":
-      msg = data['name']+" LR per LG rank:",dict.get(data['id'].__getitem__(6))
-    elif data['text'] == "!LRPerMS":
-      msg = data['name']+" LR per MS:",dict.get(data['id'].__getitem__(7))
-    elif data['text'] == "!LRPerMSrank":
-      msg = data['name']+" LR per MS rank:",dict.get(data['id'].__getitem__(8))
-    elif data['text'] == "!help":
-      msg = "!LR" + "\n" + "!LG" + "\n" + "!LRrank" + "\n" + "!LGrank" + "\n" + "!LRPerLG" + "\n" + "!LRPerLGrank" + "\n" + "!LRPerMS" + "\n" + "!LRPerMSrank" + "\n" + "!help"
-  send_message(msg)
+    if data['sender_id'] in dict.keys():
+      if data['text'] == "!LR":
+        msg = data['name']+" LR:",dict.get(data['sender_id']).__getitem__(1)
+      elif data['text'] == "!LRrank":
+        msg = data['name']+" LRrank:",dict.get(data['sender_id']).__getitem__(2)
+      elif data['text'] == "!LG":
+        msg = data['name']+" LG:",dict.get(data['sender_id']).__getitem__(3)
+      elif data['text'] == "!LGrank":
+        msg = data['name']+" LGrank:",dict.get(data['sender_id'].__getitem__(4))
+      elif data['text'] == "!LRPerLG":
+        msg = data['name']+" LR per LG:",dict.get(data['sender_id'].__getitem__(5))
+      elif data['text'] == "!LRPerLGrank":
+        msg = data['name']+" LR per LG rank:",dict.get(data['sender_id'].__getitem__(6))
+      elif data['text'] == "!LRPerMS":
+        msg = data['name']+" LR per MS:",dict.get(data['sender_id'].__getitem__(7))
+      elif data['text'] == "!LRPerMSrank":
+        msg = data['name']+" LR per MS rank:",dict.get(data['sender_id'].__getitem__(8))
+      elif data['text'] == "!help":
+        msg = "!LR" + "\n" + "!LG" + "\n" + "!LRrank" + "\n" + "!LGrank" + "\n" + "!LRPerLG" + "\n" + "!LRPerLGrank" + "\n" + "!LRPerMS" + "\n" + "!LRPerMSrank" + "\n" + "!help"
+    send_message(msg)
   # msg = 'Recieved {}'.format(data))
   # msg = '{}, you sent "{}".'.format(data['name'], data['text'])
 
