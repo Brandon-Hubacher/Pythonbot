7c26e6754088dc6c241ac5007a643c719ce57626
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-10-20 14:59:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ def webhook():
     msg = "recognizes you sent message"
     #if data['id'] in dict.keys():
     if data['text'] == "!LR":
-      msg = data['name']+" LR:",dict.get(data['id'].__getitem__(1))
+      msg = "hi "+dict.keys()
+      #msg = data['name']+" LR:",dict.get(data['id'].__getitem__(1))
     elif data['text'] == "!LR rank":
       msg = data['name']+" LR rank:",dict.get(data['id'].__getitem__(2))
     elif data['text'] == "!LG":
