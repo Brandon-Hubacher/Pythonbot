780474d6cb0415ec138d8bea81abb1e9a1b8956e
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-10-20 17:47:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,21 +42,21 @@ def webhook():
     if data['sender_id'] in dict.keys():
       if data['text'] == "!LR":
         msg = "Likes Received"+"\n"+data['name']+": "+str(dict.get(data['sender_id']).__getitem__(1))
-      elif data['text'] == "!LRrank":
+      elif data['text'] == "!LRRANK":
         msg = "Likes Received Rank"+"\n"+data['name']+": "+str(dict.get(data['sender_id']).__getitem__(2))
       elif data['text'] == "!LG":
         msg = "Likes Given"+"\n"+data['name']+": "+str(dict.get(data['sender_id']).__getitem__(3))
-      elif data['text'] == "!LGrank":
+      elif data['text'] == "!LGRANK":
         msg = "Likes Given Rank"+"\n"+data['name']+": "+str(dict.get(data['sender_id']).__getitem__(4))
-      elif data['text'] == "!LRPerLG":
+      elif data['text'] == "!LRPERLG":
         msg = "Likes Received per Likes Given"+"\n"+data['name']+": "+str(dict.get(data['sender_id']).__getitem__(5))
-      elif data['text'] == "!LRPerLGrank":
+      elif data['text'] == "!LRPERLGRANK":
         msg = "Likes Received per Likes Given Rank"+"\n"+data['name']+": "+str(dict.get(data['sender_id']).__getitem__(6))
-      elif data['text'] == "!LRPerMS":
+      elif data['text'] == "!LRPERMS":
         msg = "Likes Received per Messages Sent"+"\n"+data['name']+": "+str(dict.get(data['sender_id']).__getitem__(7))
-      elif data['text'] == "!LRPerMSrank":
+      elif data['text'] == "!LRPERMSRANK":
         msg = "Likes Received per Messages Sent Rank"+"\n"+data['name']+": "+str(dict.get(data['sender_id']).__getitem__(8))
-      elif data['text'] == "!help":
+      elif data['text'] == "!HELP":
         msg = "!LR" + "\n" + "!LG" + "\n" + "!LRrank" + "\n" + "!LGrank" + "\n" + "!LRPerLG" + "\n" + "!LRPerLGrank" + "\n" + "!LRPerMS" + "\n" + "!LRPerMSrank" + "\n" + "!help"
     send_message(msg)
   # msg = 'Recieved {}'.format(data))
