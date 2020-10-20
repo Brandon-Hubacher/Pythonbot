c59064dd5eeb1af6a14eddbbd23ebf4a9f0b7554
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-10-19 19:34:27 -0500

    Add files via upload

---
+++ FormatFilePY.py
@@ -0,0 +1,28 @@
+personDict = {}
+
+with open("C:\\Users\\brandon\\Desktop\\FinalPersonData.json", 'r') as rf:
+    for line in rf:
+        variableList = line.split(',')
+        id = variableList[0]
+        variableList.pop(0)
+        personDict[id] = variableList
+
+    #print(personDict.get("35762170"))
+    print(personDict.values())
+    print(personDict.get("35762170").__getitem__(8))
+
+
+#{
+#    "attachments": [],
+#    "avatar_url": "http://i.groupme.com/123456789",
+#    "created_at": 1302623328,
+#    "group_id": "1234567890",
+#    "id": "1234567890",
+#    "name": "John",
+#    "sender_id": "12345",
+#    "sender_type": "user",
+#    "source_guid": "GUID",
+#    "system": false,
+#    "text": "Hello world ☃☃",
+#    "user_id": "1234567890"
+#}
