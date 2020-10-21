3dfebfbc44f9c1f3dc6f3abe7c82e47b03bdff4f
Mr_Bari_99 <brandon.hubacher@gmail.com>
2020-10-21 09:31:42 -0500

    Update IDAndName.java

--- Java Code/IDAndName.java
+++ Java Code/IDAndName.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class IDAndName {
-    //LikeMapMaker LMM = new LikeMapMaker();
     public HashMap<String, Person> personMap;
 
     public IDAndName()
@@ -18,10 +17,7 @@ public class IDAndName {
     public void setPersonMap(File convoFile)
     {
         try {
-            //File file = new File("C:\\Users\\brandon\\Desktop\\gmdata\\61672113\\conversation.json");
             Scanner scan = new Scanner(convoFile);
-
-
             while(scan.hasNextLine())
             {
                 String data = scan.nextLine();
@@ -29,7 +25,6 @@ public class IDAndName {
                 for(int i = 1; i < lineArray.length; i++)
                 {
                     String[] variableArray = lineArray[i].split("\"");
-                    //System.out.println(variableArray[5]);
                     if(!(personMap.containsKey(variableArray[1])))
                     {
                         personMap.put(variableArray[1], new Person(variableArray[5]));
@@ -86,7 +81,6 @@ public class IDAndName {
                     }
                 }
 
-                //int[] rank = new int[personMap.size()];
                 tracker = 0;
                 for(String string : personMap.keySet())
                 {
@@ -143,7 +137,6 @@ public class IDAndName {
                     }
                 }
 
-                //double[] rank = new double[personMap.size()];
                 tracker = 0;
                 for(String string : personMap.keySet())
                 {
@@ -173,145 +166,6 @@ public class IDAndName {
                 writer.close();
 
             }
-
-
-
-            /*
-
-            public void setNumberOflikes()
-            {
-                int sum = 0;
-                for(String str : LMM.senderIDAndLikesReceived.keySet())
-                {
-                    for(List<String> innerList : LMM.senderIDAndLikesReceived.get(str))
-                    {
-                        sum += innerList.size();
-                    }
-                    personMap.get(str).setNumberOfLikesReceived(sum);
-                }
-            }
-
-            public void setNumberOfLikesGiven()
-            {
-                for(String str : LMM.senderIDAndLikesReceived.keySet())
-                {
-                    for(List<String> innerList : LMM.senderIDAndLikesReceived.get((str)))
-                    {
-                        for(String idnum : innerList)
-                        {
-                            personMap.get(idnum).incrementNumberOfLikesGiven();
-                        }
-                    }
-                }
-            }
-
-            public void setRankNumberOfLikesReceived()
-            {
-                int[] rank = new int[personMap.size()];
-                int tracker = 0;
-                for(String string : personMap.keySet())
-                {
-                    rank[tracker] = personMap.get(string).getNumberOfLikesReceived();
-                    tracker++;
-                }
-                Arrays.sort(rank);
-                for(int i = rank.length - 1; i >= 0; i--)
-                {
-                    for(String str : personMap.keySet())
-                    {
-                        if(personMap.get(str).getNumberOfLikesReceived() == rank[i])
-                        {
-                            personMap.get(str).setLRRank(rank.length - i);
-                        }
-                    }
-                }
-            }
-
-            public void setRankNumberOfLIkesGiven()
-            {
-                int[] rank = new int[personMap.size()];
-                int tracker = 0;
-                for(String string : personMap.keySet())
-                {
-                    rank[tracker] = personMap.get(string).getNumberOfLikesGiven();
-                    tracker++;
-                }
-                Arrays.sort(rank);
-                for(int i = rank.length - 1; i >= 0; i--)
-                {
-                    for(String str : personMap.keySet())
-                    {
-                        if(personMap.get(str).getNumberOfLikesGiven() == rank[i])
-                        {
-                            personMap.get(str).setLGRank(rank.length - i);
-                        }
-                    }
-                }
-            }
-
-            public void setFractionOfLRPerLG()
-            {
-                for(String str : personMap.keySet())
-                {
-                    double entry = (float) personMap.get(str).getNumberOfLikesReceived()/ (float) personMap.get(str).getNumberOfLikesGiven();
-                    personMap.get(str).setFractionOfLRPerLG(entry);
-                }
-            }
-
-            public void setRankLRPerLG()
-            {
-                double[] rank = new double[personMap.size()];
-                int tracker = 0;
-                for(String string : personMap.keySet())
-                {
-                    rank[tracker] = personMap.get(string).getFractionOfLRPerLG();
-                    tracker++;
-                }
-                Arrays.sort(rank);
-                for(int i = rank.length - 1; i >= 0; i--)
-                {
-                    for(String str : personMap.keySet())
-                    {
-                        if(personMap.get(str).getFractionOfLRPerLG() == rank[i])
-                        {
-                            personMap.get(str).setLRPerLGRank(rank.length - i);
-                        }
-                    }
-                }
-            }
-
-            public void setLRPerMS()
-            {
-                for(String str : personMap.keySet())
-                {
-                    double entry = (float) personMap.get(str).getNumberOfLikesReceived()/ (float) LMM.senderIDAndLikesReceived.get(str).size();
-                    personMap.get(str).setFractionOfLRPerMS(entry);
-                }
-            }
-
-            public void setRankLRPerMS()
-            {
-                double[] rank = new double[personMap.size()];
-                int tracker = 0;
-                for(String string : personMap.keySet())
-                {
-                    rank[tracker] = personMap.get(string).getFractionOfLRPerMS();
-                    tracker++;
-                }
-                Arrays.sort(rank);
-                for(int i = rank.length - 1; i >= 0; i--)
-                {
-                    for(String str : personMap.keySet())
-                    {
-                        if(personMap.get(str).getFractionOfLRPerMS() == rank[i])
-                        {
-                            personMap.get(str).setLRPerLGRank(rank.length - i);
-                        }
-                    }
-                }
-            }
-
-             */
         }
         catch(IOException e) {
             System.out.println("IDAndName didn't work");
